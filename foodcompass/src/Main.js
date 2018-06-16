33818da58a63b4dc5351dd3fd0a0ac33f6358e92
Terry Wade <terry.wade@valpo.edu>
2018-06-15 23:48:16 -0400

    Add Benefits class

--- foodcompass/src/Main.js
+++ foodcompass/src/Main.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import Option from './Option';
 import EligibilityForm from './EligibilityForm';
 
+import Benefits from './Benefits';
+
 class Main extends Component {
     state = {
         clicked: false,
@@ -15,6 +17,7 @@ class Main extends Component {
                 {this.state.clicked ?
                     <EligibilityForm /> :
                     <Option text = "Determine your Eligibility" loadForm = {this.loadForm}/>}
+                    <Benefits data = {[true,true,false,true,false,false,false,true]} />
             </div>
             
         );
